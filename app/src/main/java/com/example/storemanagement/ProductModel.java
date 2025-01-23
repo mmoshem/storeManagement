7224fa8767fb1050824a9b855e0e561a42d01e50
moshe <45424025+mmoshem@users.noreply.github.com>
2025-01-23 20:57:52 +0200

    changed prices to String + added photos

--- app/src/main/java/com/example/storemanagement/ProductModel.java
+++ app/src/main/java/com/example/storemanagement/ProductModel.java
@@ -3,6 +3,55 @@ package com.example.storemanagement;
 public class ProductModel {
     private int m_image;
     private String m_name;
-    private float m_price;
+    private String m_price;
     private int m_countItem;
+    private int m_id;
+
+    public int getM_image() {
+        return m_image;
+    }
+
+    public void setM_image(int m_image) {
+        this.m_image = m_image;
+    }
+
+    public String getM_name() {
+        return m_name;
+    }
+
+    public void setM_name(String m_name) {
+        this.m_name = m_name;
+    }
+
+    public String getM_price() {
+        return m_price;
+    }
+
+    public void setM_price(String m_price) {
+        this.m_price = m_price;
+    }
+
+    public int getM_countItem() {
+        return m_countItem;
+    }
+
+    public void setM_countItem(int m_countItem) {
+        this.m_countItem = m_countItem;
+    }
+
+    public int getM_id() {
+        return m_id;
+    }
+
+    public void setM_id(int m_id) {
+        this.m_id = m_id;
+    }
+
+    public ProductModel(int m_image, String m_name, String m_price, int m_countItem, int m_id) {
+        this.m_image = m_image;
+        this.m_name = m_name;
+        this.m_price = m_price;
+        this.m_countItem = m_countItem;
+        this.m_id = m_id;
+    }
 }
---
+++ app/src/main/java/com/example/storemanagement/StoreData.java
@@ -0,0 +1,30 @@
+package com.example.storemanagement;
+
+public class StoreData {
+    static String[] ProductArray = {
+            "cucumber", "carrot", "apple", "broccoli", "eggplant", "Purple Cabbage", "Green Cabbage ",
+            "Chinese Cabbage", "Asparagus", "Avocado", "Artichoke", "Celery", "Cauliflower", "Corn",
+            "Kale", "Garlic", "Green Onions", "Spinach", "Zucchini", "Turnip", "Red Radish", "Beetroot",
+            "Okra", "Peas", "Pumpkin", "Sweet Potato", "Mushroom"
+    };
+
+
+    static String[] price = {
+            "₪1.25", "₪0.99", "₪0.50", "₪1.75", "₪1.20", "₪2.00", "₪1.50",
+            "₪1.80", "₪2.25", "₪2.50", "₪2.10", "₪1.30", "₪1.60", "₪1.10",
+            "₪2.35", "₪0.90", "₪1.40", "₪1.70", "₪1.85", "₪0.95", "₪1.10",
+            "₪2.05", "₪1.25", "₪1.95", "₪2.15", "₪2.30", "₪1.75"
+    };
+
+    static Integer[] drawableArray = {R.drawable.cucumber, R.drawable.carrot, R.drawable.apple,
+            R.drawable.broccoli, R.drawable.eggplant, R.drawable.purple_cabbage,
+            R.drawable.green_cabbage, R.drawable.chinese_cabbage, R.drawable.asparagus,R.drawable.avocado,
+            R.drawable.artichoke, R.drawable.celery, R.drawable.cauliflower,R.drawable.corn,
+            R.drawable.kale, R.drawable.garlic, R.drawable.green_onions,R.drawable.spinach,
+            R.drawable.zucchini, R.drawable.turnip, R.drawable.red_radish,R.drawable.beetroot,
+            R.drawable.okra, R.drawable.peas, R.drawable.pumpkin,R.drawable.sweet_potato,R.drawable.mushroom
+    };
+
+    Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+            18, 19, 20, 21, 22, 23, 24, 25, 26, 27};
+}
