6d005d6f4aec3a2293eb5b4a5edb5b03db8c0bc0
moshe <45424025+mmoshem@users.noreply.github.com>
2025-01-23 16:03:59 +0200

    idk

---
+++ app/src/main/java/com/example/storemanagement/ProductModel.java
@@ -0,0 +1,8 @@
+package com.example.storemanagement;
+
+public class ProductModel {
+    private int m_image;
+    private String m_name;
+    private float m_price;
+    private int m_countItem;
+}
